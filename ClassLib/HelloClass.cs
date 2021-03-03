5c7052f7f739b82dc6bed3a9fd12902849753074
Марк Цимбалюк <tsimbalyuk_mark@live.kaznu.kz>
2021-03-03 19:10:39 +0600

    HelloClass and HelloWorld func

---
+++ ClassLib/HelloClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public static class HelloClass
+    {
+        public static int HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+            return 0;
+        }
+    }
+}
--- KaspiLabPjt/Program.cs
+++ KaspiLabPjt/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ClassLib;
 
 namespace KaspiLabPjt
 {
@@ -10,7 +7,7 @@ namespace KaspiLabPjt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ClassLib.HelloClass.HelloWorld();
             Console.ReadKey();
         }
     }
